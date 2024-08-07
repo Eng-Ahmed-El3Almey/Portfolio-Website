a60e49e39bd7fa97deeb67e2f3dc79ad73f4ef47
El3lAmey <hfarghal6@gmail.com>
2024-08-07 11:38:04 +0300

    Make hader responsive in all screens

--- js/main.js
+++ js/main.js
@@ -7,3 +7,7 @@ window.onscroll = function () {
     header.classList.remove("active");
   }
 };
+let nav_links = document.getElementById("links");
+function open_close_menu() {
+  nav_links.classList.toggle("active")
+}
